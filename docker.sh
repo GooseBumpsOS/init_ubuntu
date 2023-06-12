f515e5a7942ed37058ab4561843d56813006eb03
Georgy Gusev <32247640+GooseBumpsOS@users.noreply.github.com>
2023-06-12 22:33:32 +0300

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -7,7 +7,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 #docker compose
 sudo curl -L https://github.com/docker/compose/releases/download/v2.12.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
