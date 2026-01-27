3818699de9d4a152f3e94a54f99aebc046c0921f
Georgy Gusev <32247640+GooseBumpsOS@users.noreply.github.com>
2026-01-28 01:29:55 +0300

    Update newServer.sh

--- newServer.sh
+++ newServer.sh
@@ -9,3 +9,10 @@ sudo apt install wget -y
 sudo apt install git -y
 sudo apt install htop -y
 sudo apt install net-tools -y
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cd .oh-my-zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+cd ~
